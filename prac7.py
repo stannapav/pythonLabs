9bd12894835ed8ccb380ee2559f7a6b12587090a
Anna Striharchuk <stannapav@gmail.com>
2023-09-25 20:37:51 +0300

    prac 7 done

---
+++ prac7.py
@@ -0,0 +1,37 @@
+import random
+
+#2
+array = []
+for i in range(10):
+    array.append(random.randint(0,11))
+
+odd = [item for item in array if item % 2 == 1]
+even = [item for item in array if item % 2 == 0]
+
+print("original list:")
+print(array)
+print("odd from list:")
+print(odd)
+print("even from list:")
+print(even)
+print("\n")
+
+#3
+list = []
+min, max = -10, 9
+for i in range(10):
+    list.append(random.randint(-10,1))
+
+print("original list:")
+print(list)
+
+listenum = enumerate(list)
+
+for i, value in listenum:
+    if value < 0:
+        list[i] = min
+    else:
+        list[i] = max
+
+print("changed list:")
+print(list)
